2f969ba494d5a6ec66fbeda9052e9aa6e527236c
shao xuehai <shaoxuehai@gridsum.com>
2023-03-07 10:04:43 +0800

    update

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -17,14 +17,9 @@ require("mason-lspconfig").setup({
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require("lspconfig").lua_ls.setup {
-  capabilities = capabilities,
-}
-
 -- format on save
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
   vim.api.nvim_create_autocmd('BufWritePre', {
-    group = vim.api.nvim_create_augroup('LspFormatting', { clear = true }),
     buffer = bufnr,
     callback = function()
       vim.lsp.buf.format()
@@ -33,6 +28,8 @@ local on_attach = function(client, bufnr)
 end
 
 local lsp_config = {
+  capabilities = capabilities,
+  group = vim.api.nvim_create_augroup('LspFormatting', { clear = true }),
   on_attach = function(_, bufnr)
     on_attach(_, bufnr)
   end
@@ -42,8 +39,8 @@ require("mason-lspconfig").setup_handlers({
   function(server_name)
     require('lspconfig')[server_name].setup(lsp_config)
   end,
-  sumneko_lua = function()
-    require("lspconfig").sumneko_lua.setup(vim.tbl_extend('force', lsp_config, {
+  lua_ls = function()
+    require("lspconfig").lua_ls.setup(vim.tbl_extend('force', lsp_config, {
       settings = {
         Lua = {
           dianostics = {
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,6 @@ require'nvim-treesitter.configs'.setup {
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
 
   -- List of parsers to ignore installing (for "all")
